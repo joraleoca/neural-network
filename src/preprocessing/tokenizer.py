634f2f38f2bec810dc09366af23977632dcbad8a
Jorge Alegría <joraleoca@gmail.com>
2025-06-10 00:11:47 +0200

    feat: add English-Spanish translation example using Transformer

--- src/preprocessing/tokenizer.py
+++ src/preprocessing/tokenizer.py
@@ -6,13 +6,13 @@ from src.tensor import Tensor
 class Tokenizer:
     __slots__ = "vocab", "word2idx"
 
-    _PAD: ClassVar[str] = "<pad>"
-    _SOS: ClassVar[str] = "<sos>"
-    _EOS: ClassVar[str] = "<eos>"
-    _UNK: ClassVar[str] = "<unk>"
+    PAD: ClassVar[str] = "<pad>"
+    SOS: ClassVar[str] = "<sos>"
+    EOS: ClassVar[str] = "<eos>"
+    UNK: ClassVar[str] = "<unk>"
 
     def __init__(self, vocab: list[str]) -> None:
-        self.vocab = [self._PAD, self._SOS, self._EOS, self._UNK] + vocab
+        self.vocab = [self.PAD, self.SOS, self.EOS, self.UNK] + vocab
         self.word2idx = {word: i for i, word in enumerate(self.vocab)}
 
     def __call__(
@@ -25,16 +25,16 @@ class Tokenizer:
         tokens = []
 
         if add_sos:
-            tokens.append(self.word2idx[self._SOS])
+            tokens.append(self.word2idx[self.SOS])
 
-        unk_token = self.word2idx[self._UNK]
+        unk_token = self.word2idx[self.UNK]
         tokens.extend(self.word2idx.get(w, unk_token) for w in words)
 
         if add_eos:
-            tokens.append(self.word2idx[self._EOS])
+            tokens.append(self.word2idx[self.EOS])
 
         if len(tokens) < max_size:
-            tokens += [self.word2idx[self._PAD]] * (max_size - len(tokens))
+            tokens += [self.word2idx[self.PAD]] * (max_size - len(tokens))
 
         return Tensor(tokens)
 
@@ -45,7 +45,7 @@ class Tokenizer:
             if 0 <= (i := index.item()) < len(self.vocab):
                 word = self.vocab[i]
             else:
-                word = self._UNK
+                word = self.UNK
 
             decoded.append(word)
 
